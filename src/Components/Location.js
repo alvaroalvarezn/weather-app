e1f968c8ad27017fc1ea4ee348988b38fb5882ac
aalvarez <alvarez_navarro@hotmail.com>
2020-06-16 19:08:44 -0500

    Iniciamos curso react hasta el capitulo 14

---
+++ src/Components/Location.js
@@ -0,0 +1,7 @@
+import React from   'react';
+const Location = ()=>(
+    <div><h1>Barranquilla</h1>
+    </div>
+);
+
+export default Location;
---
+++ src/Components/WeatherData.js
@@ -0,0 +1,8 @@
+import React  from 'react';
+const WeatherData = ()=>(
+    <div>
+        <h1>Weather Data</h1>
+    </div>
+);
+
+export default WeatherData;
